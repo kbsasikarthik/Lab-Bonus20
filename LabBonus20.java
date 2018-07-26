bf4afa4d4750c6c364abab43109855284c8ebd66
kbsasikarthik <kbsasikarthik@gmail.com>
2018-07-25 23:45:40 -0400

    Initial commit for Lab - BonusLab20

---
+++ LabBonus20.java
@@ -0,0 +1,188 @@
+package labbonus20;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LabBonus20 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String printItemFormat = "%-14s\t %.2f\n", userChoice;
+		boolean isValid = false;
+		int userOption;
+		// this HashMap has a Key as String and value as Integer
+		Map<String,Double> inventory = new HashMap<>();
+		
+		ArrayList<String> itemList= new ArrayList<>();
+		ArrayList<Double> itemcost= new ArrayList<>();
+		ArrayList<Integer> itemQuantity = new ArrayList<>();
+		
+		
+		
+		// call the fillInventory method to add initial items to the empty Map
+		inventory = fillInventory(inventory);
+		while(!isValid) {
+			menuOptions();
+			try {
+			userChoice = Validator.getString(sc, "What can I do for you? ");
+			userOption = Integer.parseInt(userChoice);
+			if(userOption==1) {
+				printInventory(printItemFormat,inventory);	
+				continue;
+			}else if(userOption==2) {
+				addItem(sc,inventory, itemList, itemcost, itemQuantity, "What would you like to add to your cart?  ");
+				sc.nextLine();
+				continue;
+			}else if(userOption==3) {
+				displayShoppingCart(printItemFormat, itemList, itemcost);	
+				averagePriceOfItems(itemcost);
+				highCostItem(itemcost);
+				lowCostItem(itemcost);
+				continue;
+			}else if(userOption ==4) {
+				break;
+			}
+			}catch(Exception ex) {
+				System.out.println("Invalid input. Please try again.");
+				continue;
+			}
+			
+/*			if(userChoice.equals("list")) {
+				printInventory(printItemFormat,inventory);	
+			}else if(userChoice.equals("add")) {
+				addItem(sc,inventory, itemList, itemcost, itemQuantity, "Enter an item from the menu: ");
+			}else if(userChoice.equals("cart")) {
+				displayShoppingCart(printItemFormat, itemList, itemcost);	
+				averagePriceOfItems(itemcost);
+				highCostItem(itemcost);
+				lowCostItem(itemcost);
+			}else if(userChoice.equals("quit")) {
+				break;
+			}*/
+			isValid=Validator.continueOrNot(sc, "Would you like to see the menu again?(y/n) : ", "[yY]");
+			sc.nextLine();
+		}
+		System.out.println("\nThanks for shopping with us! See you next time!");
+	
+		
+	}
+	
+	private static void menuOptions() {
+		System.out.println("\nMenu Options\n~~~~ ~~~~~~~");
+		System.out.println("1.List of items\n2.Add items to the cart\n3.Show the cart\n4.Quit\n");
+	}
+
+
+	private static void addItem(Scanner sc, Map<String, Double> inventory,
+				    				 ArrayList<String> itemList, ArrayList<Double> itemCost,ArrayList<Integer> itemQuantity, 
+				    				 String prompt) {
+		String userInput;
+		boolean isValid=false;		
+		int count=0, quantity = 0;
+		
+		while(!isValid) {
+			System.out.print(prompt);
+			userInput = sc.next().toLowerCase();
+			//userInput = userInput.toLowerCase();
+			if(inventory.containsKey(userInput)) {
+				count++;
+				itemList.add(userInput);
+				itemCost.add(inventory.get(userInput));
+				if(itemList.contains(userInput)) {
+					quantity++;
+				itemQuantity.add(quantity);
+					
+				}else {
+					itemCost.add(inventory.get(userInput));	
+				}
+				System.out.println(userInput+" is added to the cart!");
+			} else {
+				System.out.println("Sorry! We dont carry "+userInput+" in our store!");
+				addItem(sc, inventory, itemList, itemCost, itemQuantity,prompt);
+			}
+			
+			isValid = Validator.continueOrNot(sc, "Would you like to add more items? (y/n) : ", "[yY]");
+	
+		}
+		System.out.println("You have added "+count+" items to the cart!");
+		//System.out.println("item) Quantity - "+itemQuantity);
+	}
+
+	// finds the low cost item from the cart and lists it
+	private static void lowCostItem(ArrayList<Double> itemCost) {
+		double lowCost=100;
+		for(double cost:itemCost) {
+			if(cost<lowCost) {
+				lowCost = cost;
+			}
+		}
+		System.out.printf("\nThe least expensive item in your cart costs $%.2f\n",lowCost);	
+	}
+
+	// finds the high cost item from the cart and lists it
+	private static void highCostItem(ArrayList<Double> itemCost) {
+		double highCost=0;
+		//ArrayList<Double> sortedArray = itemCost.sort();
+		for(double cost:itemCost) {
+			if(cost>highCost) {
+				highCost = cost;
+			}
+		}
+		System.out.printf("\nThe most expensive item in your cart costs $%.2f",highCost);	
+	}
+	
+	// calculates the average price of items in the cart and prints it
+	private static void averagePriceOfItems(ArrayList<Double> itemCost) {
+		double averageCost=0, totalCost=0;
+		for(double cost: itemCost) {
+			totalCost += cost;
+		}
+		System.out.printf("\nTotal Cost $%.2f",totalCost);
+		averageCost = totalCost / itemCost.size();
+		System.out.printf("\nThe average price of your cart is $%.2f",averageCost);
+	}
+
+
+	// method to display items and corresponding prices from the ArrayList that stores the information
+	private static void displayShoppingCart(String printItemFormat, ArrayList<String> shoppingList, ArrayList<Double> itemCost) {
+		System.out.println("\nItems in your cart");
+		System.out.println("~~~~~~~~~~~~~~~~~~");
+		for(int i=0;i<shoppingList.size();i++) {
+			System.out.printf(printItemFormat, shoppingList.get(i), itemCost.get(i));
+		}	
+	}
+
+
+	// method used to populate items to the HashMap with key-Value pair
+	private static Map<String,Double> fillInventory(Map<String, Double> inventory) {
+		
+		inventory.put("apple", 2.99);
+		inventory.put("banana", 0.55);
+		inventory.put("cherries", 3.99);
+		inventory.put("grapes", 2.49);
+		inventory.put("strawberry", 4.99);
+		inventory.put("blueberry", 3.99);
+		inventory.put("mango", 0.99);
+		inventory.put("grapefruit", 5.99);
+		inventory.put("kiwi", 1.99);
+		inventory.put("cucumber", 2.99);
+
+		return inventory;
+		
+	}
+	private static void printInventory(String printItemFormat, Map<String,Double> inventory) {
+		System.out.printf("%s\t\t%s\n", "Item","Price");
+		System.out.printf("%s\t\t%s\n", "~~~~","~~~~~");
+		
+		for (String item: inventory.keySet()) {
+			System.out.printf(printItemFormat,item, inventory.get(item));
+		}
+/*		for(Map.Entry<String, Double> entry : inventory.entrySet()) {
+			System.out.printf("%-14s\t %.2f\n",entry.getKey(), entry.getValue());
+		}*/
+
+	}
+}
---
+++ Validator.java
@@ -0,0 +1,186 @@
+package labbonus20;
+//@ Sasikaladevi Kumarasamy
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// this program checks for validity of many types of data, each in a different method. 
+// It even checks if the user input data is within range mentioned or not(where applicable)
+import java.util.Scanner;
+
+public class Validator {
+	Scanner sc = new Scanner(System.in);
+	// overloaded method with different set of parameters. 
+	//This checks if the user has entered an integer or not. Returns the valid number.
+	public static int getInteger(Scanner sc, String prompt) {
+		//int number;
+/*		try {
+			System.out.print(prompt);
+			return sc.nextInt();
+		} catch (InputMismatchException ex) {
+			System.out.println("Invalid number. Please enter a valid integer!");
+			sc.nextLine();
+			return getInteger(sc,prompt);
+		}*/
+		boolean isValid = false;
+		do {
+			System.out.print(prompt);
+			isValid = sc.hasNextInt();
+			if(!isValid) {
+				System.out.println("Invaild entry. Please try again!");
+			}
+		} while(!isValid);
+		return sc.nextInt();
+		
+	}
+	// overloaded method with different set of parameters. 
+	//This checks if the user has entered an integer within the range mentioned.
+
+	public static int getInteger(Scanner sc, String prompt,int min,int max) {
+		boolean isValid = false;
+		int number = 0;
+		while(!isValid) {
+			number = getInteger(sc,prompt);
+			if(number<min) {
+				System.out.println("The number has to be greater than "+min);
+				
+			}
+			else if(number >max) {
+				System.out.println("The number has to be less than "+max);
+			}
+			else {
+				isValid = true;
+			}
+				
+		} 
+		return number;
+	
+	}
+	
+	// overloaded method with different set of parameters. 
+	//This checks if the user has entered a double or not and returns a valid number.
+
+	public static double getDouble(Scanner sc, String prompt) {
+/*		try {
+			System.out.print(prompt);
+			return sc.nextDouble();
+		} catch (InputMismatchException ex) {
+			System.out.println("Invaild entry. Please try again!");
+			sc.nextLine();
+			return getDouble(sc, prompt);
+		}
+		*/
+		
+		boolean isValid = false;
+	
+		do {
+			System.out.print(prompt);
+			isValid = sc.hasNextDouble();
+			if(!isValid) {
+				System.out.println("Invaild entry. Please try again!");
+			}
+		} while(!isValid);
+		return sc.nextDouble();
+	}
+// overloaded method with different set of parameters. 
+//This checks if the user has entered a double or not and also if it is within the range mentioned.
+	public static double getDouble(Scanner sc, String prompt, double min, double max) {
+		double number=0;
+		boolean isValid = false;
+		while(!isValid) {
+			number = getDouble(sc,prompt);
+			if(number<min) {
+				System.out.println("The number has to be greater than "+min);
+			}
+			else if(number >max) {
+				System.out.println("The number has to be less than "+max);
+			}
+			else {
+				isValid = true;
+			}
+		}
+			return number;	
+	}
+	
+	// prints the prompt and returns the user entered string
+	public static String getString(Scanner sc, String prompt) {
+		System.out.print(prompt);
+		return sc.nextLine();
+	}
+	
+	// checks the user input string matches the RegEx pattern or not. 
+	// It keeps asking until valid entry is made.
+	public static String getRegExString(Scanner sc,String prompt,String regExPattern ) {
+		boolean isValid = false;
+		String userInput="";
+		while(!isValid) {
+//			System.out.print(prompt);
+			userInput = getString(sc, prompt);
+			if (!(userInput.matches(regExPattern))) 
+				System.out.println("Invalid entry! Please try again.");
+			else
+				isValid = true;
+			
+		}	
+		return userInput;
+	}
+	
+	// checks for the valid inputs for continue or not. 
+	// If the user input string matches the RegEx pattern received, it returns true; else returns false.
+	public static boolean continueOrNot(Scanner sc, String prompt, String regExPattern) {
+		boolean isValid = false;
+		while(!isValid) {
+			System.out.print(prompt);
+			String userInput = sc.next();
+			if(userInput.matches(regExPattern))
+				return false;
+			else
+				return true;
+		}
+		return isValid;		
+	}
+	
+	// checks for the input as HTML tags < > or </ > and returns the user input string 
+	// only if it matches the RegEx pattern sent; else it throws erroe.
+	public static String htmlTagValidation(Scanner sc, String prompt, String regExPattern) {
+		boolean isValid = false;
+		String userInput="";
+		while(!isValid) {
+			System.out.print(prompt);
+			userInput = sc.next();
+			if(!(userInput.matches(regExPattern)))
+				System.out.println("Invalid entry. Please try again!");
+			else
+				isValid=true;
+		}
+		return userInput;		
+	}
+// accepts a string and converts it to Date type and returns it
+	public static Date getDate(Scanner sc, String prompt) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+		format.setLenient(false);
+		boolean isValid = false;
+		Date date=null;
+		String userInput="";
+		
+		while(!isValid) {
+			// get in the raw string
+			userInput = getString(sc, prompt);
+			//convert the raw string to date format
+			try {
+				date = format.parse(userInput);
+				isValid = true;
+			} catch (ParseException e) {
+				//e.printStackTrace();
+				isValid = false;
+				System.out.println("Cannot convert "+userInput+" to date!");
+			}
+//			sc.nextLine();
+		}	
+		return date;
+	}
+		
+
+}
